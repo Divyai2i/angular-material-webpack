9e07cd4399507d8fe902539288b621fa98983a77
Gonzalo Bahamondez <gonzalo.bahamondez.c@gmail.com>
2016-02-11 18:59:40 -0300

    adding components inside dialog

--- public/src/js/FormDialogController.js
+++ public/src/js/FormDialogController.js
@@ -31,6 +31,14 @@ module.exports = angular.module("app.formdialog", [
 }])
 
 .controller("FormDialogController", function ($scope, $mdDialog, $mdMedia) {
+  $scope.userState = "";
+
+  $scope.states = ("AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS " +
+      "MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI " +
+      "WY").split(" ").map(function (state) { return { abbrev: state }; });
+
+  console.log($scope.states);
+  
   $scope.showAdvanced = function(ev) {
    var useFullScreen = ($mdMedia("sm") || $mdMedia("xs"))  && $scope.customFullscreen;
    $mdDialog.show({
