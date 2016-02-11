649a0005844c8aea31fd8288f4036040d77a1aef
Gonzalo Bahamondez <gonzalo.bahamondez.c@gmail.com>
2016-02-10 23:37:50 -0300

    pruebas

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,7 +4,8 @@ require("../css/style.css");
 require("../css/slide-effect.css");
 require("angular-material/angular-material.css");
 
-var angular = require("angular");
+//hola
+var angular  = require("angular");
 var ngRoute = require("angular-route");
 var ngAnimate = require("angular-animate");
 var ngAria = require("angular-aria");
